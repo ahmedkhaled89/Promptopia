77f15044b441da7a8080dcc4ce34ae6e1becede0
ahmedkhaled <ahmedkhaled8901@gmail.com>
2024-10-13 02:38:07 +0300

    feat: Add Mobile Navigation Styles

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,9 +1,11 @@
 'use client';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useState } from 'react';
 
 const Nav = () => {
-  const isUserLogedIn = true;
+  const isUserLoggedIn = false;
+  const [toggleDropDown, setToggleDropDown] = useState(false);
 
   return (
     <nav className='flex-between w-full mb-16'>
@@ -20,7 +22,7 @@ const Nav = () => {
 
       {/* Desktop Navigation */}
       <div className='sm:flex hidden'>
-        {isUserLogedIn ? (
+        {isUserLoggedIn ? (
           <div className='flex gap-3 md:gap-5'>
             <Link href='/create-prompt' className='black_btn'>
               Create Post
@@ -37,6 +39,54 @@ const Nav = () => {
                 className='rounded-full'
               />
             </Link>
+          </div>
+        ) : (
+          <>
+            <button type='button' className='black_btn'>
+              Sing In
+            </button>
+          </>
+        )}
+      </div>
+      {/* Mobil Navigation */}
+      <div className='sm:hidden flex relative'>
+        {isUserLoggedIn ? (
+          <div className='flex'>
+            <Image
+              alt='Profile Pic'
+              width={37}
+              height={37}
+              src='/assets/images/logo.svg'
+              onClick={() => {
+                setToggleDropDown((prev) => !prev);
+              }}
+            />
+
+            {toggleDropDown && (
+              <div className='dropdown'>
+                <Link
+                  href='/profile'
+                  className='dropdown_link'
+                  onClick={() => setToggleDropDown(false)}
+                >
+                  My Profile
+                </Link>
+                <Link
+                  href='/create-prompt'
+                  className='dropdown_link'
+                  onClick={() => setToggleDropDown(false)}
+                >
+                  Create New Prompt
+                </Link>
+                <button
+                  type='button'
+                  onClick={() => setToggleDropDown(false)}
+                  className='mt-5 w-full black_btn'
+                >
+                  Sign Out
+                </button>
+              </div>
+            )}
           </div>
         ) : (
           <>
