ae22db30b62c56cac3ffc479ae65b3afe5ef772f
ahmedkhaled <ahmedkhaled8901@gmail.com>
2024-10-13 02:10:16 +0300

    feat: Add Desktop Navigation to Nav component

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,7 +1,10 @@
 'use client';
 import Image from 'next/image';
 import Link from 'next/link';
-function Nav() {
+
+const Nav = () => {
+  const isUserLogedIn = true;
+
   return (
     <nav className='flex-between w-full mb-16'>
       <Link href='/' className='flex gap-2'>
@@ -14,8 +17,37 @@ function Nav() {
         />
         <p className='logo_text'>Promptopia</p>
       </Link>
+
+      {/* Desktop Navigation */}
+      <div className='sm:flex hidden'>
+        {isUserLogedIn ? (
+          <div className='flex gap-3 md:gap-5'>
+            <Link href='/create-prompt' className='black_btn'>
+              Create Post
+            </Link>
+            <button type='button' className='outline_btn'>
+              Sign Out
+            </button>
+            <Link href='/profile'>
+              <Image
+                src='/assets/images/logo.svg'
+                width={37}
+                height={37}
+                alt='Profile image'
+                className='rounded-full'
+              />
+            </Link>
+          </div>
+        ) : (
+          <>
+            <button type='button' className='black_btn'>
+              Sing In
+            </button>
+          </>
+        )}
+      </div>
     </nav>
   );
-}
+};
 
 export default Nav;
